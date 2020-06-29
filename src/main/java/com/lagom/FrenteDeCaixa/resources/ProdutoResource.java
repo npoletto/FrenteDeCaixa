adb1ab1f7a4d1e8e093656133989c2329fb2c790
Nicolai Poletto <nicolaipoletto@yahoo.com.br>
2020-06-29 10:57:28 -0300

    Novas alterações para usar no escritorio

--- src/main/java/com/lagom/FrenteDeCaixa/resources/ProdutoResource.java
+++ src/main/java/com/lagom/FrenteDeCaixa/resources/ProdutoResource.java
@@ -1,5 +1,33 @@
 package com.lagom.FrenteDeCaixa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lagom.FrenteDeCaixa.domain.Produto;
+import com.lagom.FrenteDeCaixa.services.ProdutoService;
+
+@RestController
+@RequestMapping(value="/produtos")
 public class ProdutoResource {
 
+	@Autowired
+	private ProdutoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find (@PathVariable Integer id) {
+		Produto obj = service.find(id);
+		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<?> findAll () {
+		List<Produto > produtos = service.findAll();
+		return ResponseEntity.ok(produtos);
+	}
 }
--- src/main/java/com/lagom/FrenteDeCaixa/services/ProdutoService.java
+++ src/main/java/com/lagom/FrenteDeCaixa/services/ProdutoService.java
@@ -1,5 +1,27 @@
 package com.lagom.FrenteDeCaixa.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lagom.FrenteDeCaixa.domain.Produto;
+import com.lagom.FrenteDeCaixa.repositories.ProdutoRepository;
+
+@Service
 public class ProdutoService {
 
+	 	@Autowired
+		private ProdutoRepository repo;
+		
+		public Produto find(Integer id) {
+			Optional<Produto> obj = repo.findById(id);
+			return obj.orElse(null);
+		}
+		
+		public List<Produto> findAll() {
+			return repo.findAll();
+		}
+	
 }
